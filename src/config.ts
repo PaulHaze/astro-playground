5d77cd152045a06603dcdd96657362bf70d9c430
PaulHaze <dev@artemist.com.au>
2024-07-08 13:28:40 +1000

    add glass hover effect

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-//! CHANGETHIS
+//!CHANGETHIS
 export const SITE_TITLE = 'Astro Playground';
 export const SITE_DESCRIPTION =
 	'A place for experiments, code practise and boundary pushing';
